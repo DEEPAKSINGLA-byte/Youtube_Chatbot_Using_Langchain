ebcd1c5d01bbb3615bd4c9edc02b54a79babb5b0
deepaksingla8059924923-byte <239532455+deepaksingla8059924923-byte@users.noreply.github.com>
2026-07-30 01:25:09 +0530

    Clean project setup

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask,render_template,request,jsonify
+from rag import ask_rag
 app=Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+@app.route("/ask",methods=["POST"])
+def ask():
+    data=request.get_json()
+    video=data["video_id"]
+    question=data["question"]
+    print("Video ID:", video)
+    print("Question:", question)
+    answer=ask_rag(video,question)
+    return jsonify({
+        "answer":answer
+    })
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,1 +1,44 @@
-console.log("JavaScript Loaded");
+const button = document.getElementById("ask-btn");
+button.addEventListener("click", askQuestion);
+
+async function askQuestion() {
+
+    const video = document.getElementById("video").value;
+    const question = document.getElementById("question").value;
+    document.getElementById("answer").innerText = "Thinking...";
+    button.disabled = true;
+    button.innerText = "Thinking...";
+
+    try {
+        const response = await fetch("/ask", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+
+                video_id: video,
+                question: question
+
+            })
+
+        });
+
+        const data = await response.json();
+
+        document.getElementById("answer").innerText =
+            data.answer;
+    }
+    catch(err){
+
+        document.getElementById("answer").innerText =
+            "Something went wrong.";
+
+    }
+
+    button.disabled = false;
+    button.innerText = "Ask";
+}
